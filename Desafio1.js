b5926a1098b57b8891c6c2ae1c71a55f35e368e1
Tomas Alvarez <alvareztomas2408@gmail.com>
2022-02-23 15:20:03 -0300

    desafio1

---
+++ Desafio1.js
@@ -0,0 +1,51 @@
+class Usuario {
+            constructor (name, lastName){
+                this.name = name;
+                this.lastName = lastName;
+                this.books = [];
+                this.pets = [] ;
+            }
+ 
+            getFullName = () => `El nombre del Usuario es ${this.name} ${this.lastName}`;
+
+            addPet = (pet) => this.pets.push(pet);
+            
+            countPets = () => this.pets.length ;
+
+            addBook = (name, autor) => this.books.push(name, autor) ;
+
+            getBookNames(){
+                let namesBooks =this.books.map(book=>{return book.name;});
+                return namesBooks}
+
+        getPets = () => this.pets.map((pet) => pet.name);
+
+}
+
+let books = [
+{name: "don quijote de la mancha", autor:"Miguel de Cervantes"},
+{name:"Martin Fierro", autor:"José Hernández"},
+{name:"padre rico padre pobre", autor:"Robert Kiyosaki & Sharon Lechter"}
+
+];
+
+let pets = ["homer", "coco", "biscuit"];
+
+let usuario1 = new Usuario("Pedro", "Pascal", books, pets);
+let usuario2 = new Usuario("Ezequiel", "Perez", books, pets)
+
+usuario1.addBook ("don quijote de la mancha", "Miguel de Cervantes");
+usuario1.addBook ("Martin Fierro", "José Hernández");
+usuario1.addPet ("biscuit");
+
+usuario2.addBook("padre rico padre pobre","Robert Kiyosaki & Sharon Lechter");
+usuario2.addPet ("coco");
+usuario2.addPet ("homer");
+
+
+console.log (usuario1.getFullName());
+console.log (pets);
+console.log(books);
+console.log(usuario1.countPets());
+console.log(usuario1.getPets());
+console.log(usuario2.getBookNames());
